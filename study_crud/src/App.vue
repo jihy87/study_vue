c2813030b9245f266d3ccbaf5e48f996b3613d3c
hyji1 <hyji1@DESKTOP-239DPIM>
2021-02-09 17:04:01 +0900

    Vue CRUD 스터디 화면 생성.

---
+++ study_crud/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="wrap">
+      <Login/>
+      <Header/>
+      <Container/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Container from './components/Container.vue'
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Container,
+    Login,
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap');
+* {
+  margin:0;
+  padding:0;
+}
+ol,ul{list-style:none;}
+body {
+  font-family: 'Do Hyeon', sans-serif;
+  color: #2c3e50;
+  font-size:20px;
+  line-height:1.4;
+  background-color:#000;
+}
+#app {
+  display:flex;
+  justify-content: center;
+  align-items:center;
+  height:100vh;
+}
+.wrap {
+  width:600px;
+  background-color:#fff;
+  border-radius:10px;
+}
+</style>
