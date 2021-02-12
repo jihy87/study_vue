5450a6e240d616af7728e39edad0afcfd335ac2b
jihy <jihy.front@gmail.com>
2021-02-12 17:59:50 +0900

    vue 인프런 스터디

---
+++ learn-vue-js/vue-cli/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <app-header v-bind:props속성 이름="상위 컴포넌트의 데이터 이름">{{ str }}</app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import appHeader from "./components/AppHeader.vue";
+
+export default {
+  data: function () {
+    return {
+      str: "Header!",
+    };
+  },
+  components: {
+    "app-header": appHeader,
+  },
+  methods: {
+    renewStr: function () {
+      this.str = "hi";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
