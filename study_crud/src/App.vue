5c66d5e50be203d6f44c2d8c96f32cca532cff7f
jihy <jihy.front@gmail.com>
2021-02-16 00:32:04 +0900

    vue-router 실습시작

--- study_crud/src/App.vue
+++ study_crud/src/App.vue
@@ -1,51 +1,54 @@
 <template>
   <div id="app">
     <div class="wrap">
-      <Login/>
-      <Header/>
-      <Container/>
+      <Login />
+      <Header />
+      <Container />
     </div>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Container from './components/Container.vue'
-import Login from './components/Login.vue'
+import Header from "./components/Header.vue";
+import Container from "./components/Container.vue";
+import Login from "./components/Login.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Container,
     Login,
-  }
-}
+  },
+};
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap");
 * {
-  margin:0;
-  padding:0;
+  margin: 0;
+  padding: 0;
 }
-ol,ul{list-style:none;}
+ol,
+ul {
+  list-style: none;
+}
 body {
-  font-family: 'Do Hyeon', sans-serif;
+  font-family: "Do Hyeon", sans-serif;
   color: #2c3e50;
-  font-size:20px;
-  line-height:1.4;
-  background-color:#000;
+  font-size: 20px;
+  line-height: 1.4;
+  background-color: #000;
 }
 #app {
-  display:flex;
+  display: flex;
   justify-content: center;
-  align-items:center;
-  height:100vh;
+  align-items: center;
+  height: 100vh;
 }
 .wrap {
-  width:600px;
-  background-color:#fff;
-  border-radius:10px;
+  width: 600px;
+  background-color: #fff;
+  border-radius: 10px;
 }
 </style>
