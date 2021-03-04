4369d726a3afd2e06d3b562bb5bceab89ed11e78
hyeyeon <jihy.front@gmail.com>
2021-03-04 23:16:53 +0900

    라우터 코드 스플리팅, 모드, 없는페이지 설정.

--- study_crud/src/routes/index.js
+++ study_crud/src/routes/index.js
@@ -1,28 +1,45 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Login from "./../views/Login.vue";
-import Signup from "./../views/Signup.vue";
-import List from "./../views/List.vue";
-import Write from "./../views/Write.vue";
+// 코드 스플리팅으로 쓰지 않는 import. routes>component에 import 선언으로 교체.
+// import Login from "./../views/Login.vue";
+// import Signup from "./../views/Signup.vue";
+// import List from "./../views/List.vue";
+// import Write from "./../views/Write.vue";
 
 Vue.use(VueRouter);
 export default new VueRouter({
+  // 해쉬뱅 모드 제거를 위한 mode 재 설정 
+  // #가 붙어있는 이유: 해당 html만 서버에서 체크하기 때문.
+  // 주의할 점 : 배포 시 서버 url 설정 필요.
+  mode:'history',
   routes:[
     {
+      // 리다이렉트 설정 (기본페이지)
+      path:'/',
+      redirect:'/list',
+    },
+    {
       path:'/login',
-      component: Login
+      // 코드 스플리팅. 처음부터 페이지를 다 불러오지 않고, uri가 바뀔 때마다 호출하기. 초기 애플리케이션 속도를 높임.
+      //component: Login
+      component: () => import('./../views/Login.vue')
     },
     {
       path:'/signup',
-      component: Signup
+      component: () => import('./../views/Signup.vue')
     },
     {
       path:'/list',
-      component: List
+      component: () => import('./../views/List.vue')
     },
     {
       path:'/write',
-      component: Write
+      component: () => import('./../views/Write.vue')
+    },
+    {
+      // 정의되지 않는 경로 설정
+      path:'*',
+      component: ()=> import('./../views/NotFound.vue')
     }
   ]
 });
---
+++ study_crud/src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Page is Not found</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
