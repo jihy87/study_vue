c1e72612c3ba80af96d664f2984d621665aeba38
hyji1 <hyji1@DESKTOP-239DPIM>
2021-03-09 18:20:05 +0900

    todo앱 구현 재실습을 위한 코드 초기화.

---
+++ study_crud/src/components/todo/TodoInput_bak.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bar_form todo">
+    <!-- vue api 중 v-model 디렉티브는 입력되는 값을 바로바로 인스턴스에 매핑한다. data사용 -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <!-- vue api 중 v-on 디렉티브는 methods 내부에 함수를 선언하여 이를 받아 처리한다 -->
+    <button class="btn" v-on:click="addTodo"><i class="fas fa-plus"></i></button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      newTodoItem: "",
+    }
+  },
+  methods: {
+    addTodo: function() {
+      // console.log(this);
+      console.log(this.newTodoItem);
+      // item 저장. setItem으로 저장한다. storage.setItem(keyName, keyValue); https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+      // localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      if(this.newTodoItem !== '') {
+        // this.$emit()는 이벤트를 상위로 올린다.
+        // this.$emit('이벤트 이름', 인자1, 인자2, ...)
+        this.$emit('addTodoItem', this.newTodoItem)
+        this.clearInput();
+      }
+    },
+    clearInput: function() {
+      // input clear
+      this.newTodoItem = '';
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- study_crud/src/components/todo/TodoList.vue
+++ study_crud/src/components/todo/TodoList.vue
@@ -2,53 +2,27 @@
   <div class="list">
     <div class="list_head">
       <button class="btn">Finish로 이동</button>
-      <button class="btn" v-on:click="clearTodo">clear all</button>
+      <button class="btn">clear all</button>
     </div>
     <ul>
-      <!-- <li v-for="todoItem in todoItems" v-bind:key="todoItem"> -->
-      <!-- v-for에는 내장 index가 있다. -->
-      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item">
+      <li>
         <div class="item">
-          <span v-on:click="toggleComplete(todoItem, index)" v-bind:class="{done: todoItem.completed}">
-            <i class="far" v-bind:class="{'fa-check-square': todoItem.completed, 'fa-square': !todoItem.completed}"></i>
-            <!-- <span>{{ todoItem }}</span> -->
-            <!-- todoItem completed 속성이 true일 때 class 삽입. -->
-            {{ todoItem.item }}
-          </span>
-        </div>
-        <div class="btns">
-          <button class="btn"><i class="fas fa-pen"></i></button>
-          <button class="btn" v-on:click="removeTodo(todoItem, index)"><i class="fas fa-trash"></i></button>
-        </div>
-      </li>
-      <!-- <li>
-        <div class="item">
-          <input type="checkbox" />
-          <span>가나다라마바사</span>
+          <i class="far fa-square"></i>
+          <i class="far fa-check-square"></i>
+          <span>asdfasdf</span>
         </div>
         <div class="btns">
           <button class="btn"><i class="fas fa-pen"></i></button>
           <button class="btn"><i class="fas fa-trash"></i></button>
         </div>
-      </li> -->
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['propsdata'],
-  methods: {
-    removeTodo: function(todoItem, index) {
-      this.$emit('removeItem',todoItem, index)
-    },
-    toggleComplete: function(todoItem, index) {
-      this.$emit('toggleItem',todoItem, index)
-    },
-    clearTodo: function() {
-      this.$emit('clearAll')
-    }
-  }
+
 };
 </script>
 
